9ae548c9db37112ca1e99ac3b2e72049d24126eb
Dell <mollanoman2019@gmail.com>
2019-11-18 13:43:36 +0600

    Date Time Picker Update

--- app/src/main/java/com/example/datetimepicker/MainActivity.java
+++ app/src/main/java/com/example/datetimepicker/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.datetimepicker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Button button;
+    DatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=findViewById(R.id.textviewId);
+        button=findViewById(R.id.buttonId);
+        datePicker=findViewById(R.id.datepickerId);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(noman());
+            }
+        });
+
+    }
+
+    String noman(){
+
+        StringBuilder stringBuilder=new StringBuilder();
+        stringBuilder.append(datePicker.getDayOfMonth()+"/");
+        stringBuilder.append(datePicker.getMonth()+1+"/");
+        stringBuilder.append(datePicker.getYear());
+        return stringBuilder.toString();
+
+
     }
 }
